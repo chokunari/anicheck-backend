63dbabd6c397def5fc1b56499daf83cf598e908c
堀田直也 <chokunari3@gmail.com>
2021-02-14 06:22:35 +0000

    firstコミット

---
+++ src/index.js
@@ -0,0 +1,54 @@
+//import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// define the Express app
+const app = express();
+
+// the database
+const questions = [];
+
+// enhance your app security with Helmet
+app.use(helmet());
+
+// use bodyParser to parse application/json content-type
+app.use(bodyParser.json());
+
+// enable all CORS requests
+app.use(cors());
+
+// log HTTP requests
+app.use(morgan('combined'));
+
+// アニメ公式サイトURLをfrontendから受け取りOGPの画像srcを取得、frontendに返却する。
+app.get('/', (req, res) => {
+  const qs = questions.map(q => ({
+    id: q.id,
+    title: q.title,
+    description: q.description,
+    answers: q.answers.length,
+  }));
+  res.send(qs);
+});
+
+// insert a new question
+app.post('/', (req, res) => {
+  const {title, description} = req.body;
+  const newQuestion = {
+    id: questions.length + 1,
+    title,
+    description,
+    answers: [],
+  };
+  questions.push(newQuestion);
+  res.status(200).send();
+});
+
+
+// start the server
+app.listen(4000, () => {
+  console.log('listening on port 4000');
+});
